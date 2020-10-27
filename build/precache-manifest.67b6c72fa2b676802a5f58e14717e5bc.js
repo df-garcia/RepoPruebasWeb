1d830de9fde58c7e2071cd6abb0c9dd2d36b1c85
df-garcia <df.garcia@uniandes.edu.co>
2020-10-27 08:00:23 -0500

    upload build

---
+++ build/precache-manifest.67b6c72fa2b676802a5f58e14717e5bc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d185cb42b5fc59401d8feeaba3f3e184",
+    "url": "/index.html"
+  },
+  {
+    "revision": "549f72f3f29abb46743f",
+    "url": "/static/js/2.6f8573d6.chunk.js"
+  },
+  {
+    "revision": "d5f22024f3313f48040924a4cd45a4ed",
+    "url": "/static/js/2.6f8573d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cd8cbefd09e6c239c94",
+    "url": "/static/js/main.451a4fd8.chunk.js"
+  },
+  {
+    "revision": "3922ac85c8a3a5c2831f",
+    "url": "/static/js/runtime-main.49cfa679.js"
+  },
+  {
+    "revision": "9f71c0842a08d77decf741300c686051",
+    "url": "/static/media/ConectaTE.9f71c084.png"
+  },
+  {
+    "revision": "39e9b39628e745a39f900dc14ee4d9a7",
+    "url": "/static/media/avatar.39e9b396.jpg"
+  },
+  {
+    "revision": "ec2f9e1d7f9cdbb4bcebfe35de9ad59a",
+    "url": "/static/media/conectate_logo.ec2f9e1d.png"
+  },
+  {
+    "revision": "abe9fc02298bf2782634a59cd3d218cd",
+    "url": "/static/media/video.abe9fc02.png"
+  }
+]);
